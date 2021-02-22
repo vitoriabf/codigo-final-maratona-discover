93e11773a4b706b753dca9542e61e7de5b1d34a3
Vitoria Fontoura <vitoriabfontoura@gmail.com>
2021-02-21 22:23:41 -0300

    correção: valor de saída diferente com alguns números decimais

--- script.js
+++ script.js
@@ -141,7 +141,7 @@ const DOM = {
 const Utils = {
     formatAmount(value) {
         value = Number(value) * 100
-        return value
+        return Math.round(value)
     },
 
     formatDate(date) {
